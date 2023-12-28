494f0bf892678d3beccdffaec1700332ca54f1a9
PyDanny21 <quansahd577@gmail.com>
2023-12-28 21:23:08 +0000

    Added new features

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const Menu=document.getElementById('menu');
 const text=document.getElementById('Text');
+const settings=document.querySelector('.settings');
 const notificationBtn=document.getElementById('notification');
 const notification=document.querySelector('.notification');
-const settings=document.querySelector('.settings');
 
-
-notificationBtn.addEventListener('click',()=>{
+function toggleNote() {
     const mess=notification.classList.toggle('more-height');
-});
+};
 
-Menu.addEventListener('click',()=>{
+function toggleMenu() {
     if (window.innerWidth>700) {        
         const toggled=settings.classList.toggle('settings-large-width');
         // const show=toggled? settings.style.display='block':settings.style.display='none';
@@ -19,31 +18,34 @@ Menu.addEventListener('click',()=>{
         // const show=toggled? settings.style.display='block':settings.style.display='none';
         const menuText=toggled? text.innerHTML='&times;':text.innerHTML='&equiv;';
     };
+};
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    if (window.innerWidth<700) {        
+        $(document).ready(function () {
+            var lastScrollTop=0;
+        
+            // Detect scroll event
+            $(window).scroll(function (event) {
+                var st=$(this).scrollTop();
+        
+                // compare scroll position
+                if (st>lastScrollTop) {
+                    $('#bottomNav').slideUp();
+                } else {
+                    $('#bottomNav').slideDown();
+                }
+        
+                lastScrollTop=st;
+                
+            });
+            
+        });
+    };
+
 
 
 });
 
 
-
-
-if (window.innerWidth<700) {        
-    $(document).ready(function () {
-        var lastScrollTop=0;
-    
-        // Detect scroll event
-        $(window).scroll(function (event) {
-            var st=$(this).scrollTop();
-    
-            // compare scroll position
-            if (st>lastScrollTop) {
-                $('#bottomNav').slideUp();
-            } else {
-                $('#bottomNav').slideDown();
-            }
-    
-            lastScrollTop=st;
-            
-        });
-        
-    });
-};
