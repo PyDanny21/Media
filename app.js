347cefcf0b065d5cd92ebc46cabc8c9490dedfd3
PyDanny21 <quansahd577@gmail.com>
2023-12-28 16:55:55 +0000

    Added new features

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-$(document).ready(function () {
-    var lastScrollTop=0;
+const Menu=document.getElementById('menu');
+const text=document.getElementById('Text');
+const notificationBtn=document.getElementById('notification');
+const notification=document.querySelector('.notification');
+const settings=document.querySelector('.settings');
 
-    // Detect scroll event
-    $(window).scroll(function (event) {
-        var st=$(this).scrollTop();
 
-        // compare scroll position
-        if (st>lastScrollTop) {
-            $('#bottomNav').slideUp();
-        } else {
-            $('#bottomNav').slideDown();
-        }
+notificationBtn.addEventListener('click',()=>{
+    const mess=notification.classList.toggle('more-height');
+});
 
-        lastScrollTop=st;
+Menu.addEventListener('click',()=>{
+    if (window.innerWidth>700) {        
+        const toggled=settings.classList.toggle('settings-large-width');
+        // const show=toggled? settings.style.display='block':settings.style.display='none';
+        const menuText=toggled? text.innerHTML='&times;':text.innerHTML='&equiv;';
+    } else {
+        const toggled=settings.classList.toggle('settings-width');        
+        // const show=toggled? settings.style.display='block':settings.style.display='none';
+        const menuText=toggled? text.innerHTML='&times;':text.innerHTML='&equiv;';
+    };
+
+
+});
+
+
+
+
+if (window.innerWidth<700) {        
+    $(document).ready(function () {
+        var lastScrollTop=0;
+    
+        // Detect scroll event
+        $(window).scroll(function (event) {
+            var st=$(this).scrollTop();
+    
+            // compare scroll position
+            if (st>lastScrollTop) {
+                $('#bottomNav').slideUp();
+            } else {
+                $('#bottomNav').slideDown();
+            }
+    
+            lastScrollTop=st;
+            
+        });
         
     });
-    
-});
+};
